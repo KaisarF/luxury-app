57d501dfd9517a34f5d84eeb04aef806451666b7
unknown <kaisar0354@gmail.com>
2025-01-09 13:20:43 +0700

    fix responsive

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,6 +1,6 @@
 <div class="hero-section">
         <div class="hero-description">
-            <h1>@yield('header') <span>@yield('headerSpan')</span></h1>
+            <h1>@yield('header') <span>@yield('headerSpan')</span>@yield('secondHeader')</h1>
             <p>Analisis barang mewah dan koleksi kesayangan Anda. Keahlian kami lebih dari 49 merek ternama</p>
             
                 <a>Cek Sekarang <img src="image/search.svg" class="search-logo" alt=""></a>
